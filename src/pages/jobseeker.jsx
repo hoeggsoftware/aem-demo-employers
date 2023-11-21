5d60f9495a7bad939b388be2773619b97d967a4e
youngderrick82 <youngderrick82@gmail.com>
2023-11-21 13:04:32 -0600

    added SVGs

--- src/pages/jobseeker.jsx
+++ src/pages/jobseeker.jsx
@@ -1,29 +1,39 @@
-import * as React from 'react'
-import Layout from '@/components/Layout'
-import './App.css'
-import Benefits from '../components/benefits'
-
-
+import * as React from "react";
+import Layout from "@/components/Layout";
+import "./App.css";
+import Benefits from "../components/benefits";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 const JobSeeker = () => {
-
-
-    return (
-        <Layout>
-        <div className="sub-menu">
-            <div className="sub-menu-container">
-                <div className="container">
-                    <h1 className="sub-menu-header">Job Seekers</h1>
-                    <ul className="sub-list">
-                        <li className="sub-list-item">Placeholder 1: replace with card (possibly tabbed)</li>
-                        <li className="sub-list-item">Placeholder 2: replace with card (possibly tabbed)</li>
-                    </ul>
-                </div>
-            </div>
-            <Benefits />
+  return (
+    <Layout>
+      <div className="sub-menu">
+        <div className="sub-menu-container">
+          <div className="container">
+            <h1 className="sub-menu-header">Job Seekers</h1>
+            <ul className="sub-list">
+              <Card className="sub-list-item">
+                <CardContent >
+                  <li >
+                    Lost My Job
+                  </li>
+                </CardContent>
+              </Card>
+              <Card className="sub-list-item">
+                <CardContent>
+                  <li >
+                    Job Search
+                  </li>
+                </CardContent>
+              </Card>
+            </ul>
+          </div>
         </div>
-        </Layout>
-    )
-}
+        <Benefits />
+      </div>
+    </Layout>
+  );
+};
 
-export default JobSeeker
+export default JobSeeker;
