be7806e065a5fd128251ff355623745680ae2801
youngderrick82 <youngderrick82@gmail.com>
2023-11-21 07:45:07 -0600

    Added benefits to job seeker page

---
+++ src/pages/benefits.jsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import "./App.css";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
+
+const Benefits = () => {
+  return (
+    <div className="container">
+      <h2 className="benefits-header">Benefits</h2>
+      <h3>***An instance of each item below will automatically be injected with JS using the MAP function.***</h3>
+      <div className="accordion-container">
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Unemployment Insurance Benefit </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Unemployment Allowance</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>The European Union and Unemployment Insurance</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Redundancy Benefit</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>Insolvency Benefit</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default Benefits;
--- src/pages/jobseeker.jsx
+++ src/pages/jobseeker.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '@/components/Layout'
 import './App.css'
+import Benefits from './benefits'
 
 
 
@@ -10,13 +11,16 @@ const JobSeeker = () => {
     return (
         <Layout>
         <div className="sub-menu">
-            <h1>Job Seekers</h1>
             <div className="sub-menu-container">
-                <ul className="sub-list">
-                    <li className="sub-list-item">Placeholder 1: replace with card</li>
-                    <li className="sub-list-item">Placeholder 2: replace with card</li>
-                </ul>
+                <div className="container">
+                    <h1 className="sub-menu-header">Job Seekers</h1>
+                    <ul className="sub-list">
+                        <li className="sub-list-item">Placeholder 1: replace with card (possibly tabbed)</li>
+                        <li className="sub-list-item">Placeholder 2: replace with card (possibly tabbed)</li>
+                    </ul>
+                </div>
             </div>
+            <Benefits />
         </div>
         </Layout>
     )
