c7bec9830ff297e390da544fdf7b92226c18ff4c
youngderrick82 <youngderrick82@gmail.com>
2023-11-27 20:45:38 -0600

    Replaced carousel with better functioning one and updated homepage layout

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,50 +1,34 @@
-import { useRef, useEffect } from "react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css/bundle";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import Slider from "react-slick";
+import sliderItems from "./mockups/slideritems";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = () => {
-  const swiperElRef = useRef(null);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
 
   return (
-
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={2}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper>
-
-    /* <swiper-container
-            ref={swiperElRef}
-            slides-per-view="1"
-            loop="true"
-            speed="500"
-            navigation="true"
-            pagination="true"
-            autoplay={{delay: 1, disableOnInteraction: false}}
-            className="banner-carousel"
-          >
-            <swiper-slide>
-              slide 1
-            </swiper-slide>
-            <swiper-slide>
-              slide 2
-            </swiper-slide>
-            <swiper-slide>
-              slide 3
-            </swiper-slide>
-           
-          </swiper-container> */
+    <div className="slider-container">
+        <Slider {...settings}>
+          {sliderItems.map((d) => (
+            <div
+              key={d.title}
+              className=""
+            >
+              <div className="">
+                <img src={d.image} alt="" className="slick-image" />
+              </div>
+            </div>
+          ))}
+          </Slider>
+    </div>
   );
 };
 
---
+++ src/components/mockups/slideritems.js
@@ -0,0 +1,20 @@
+const sliderItems = [
+    {
+        "image": "https://loremflickr.com/500/240/work",
+        "title": "Image Placeholder1",
+    },
+    {
+        "image": "https://loremflickr.com/500/240/cat",
+        "title": "Image Placeholder2",
+    },
+    {
+        "image": "https://loremflickr.com/500/240/dog",
+        "title": "Image Placeholder3",
+    },
+    {
+        "image": "https://loremflickr.com/500/240/escalator",
+        "title": "Image Placeholder4",
+    }
+]
+
+export default sliderItems
