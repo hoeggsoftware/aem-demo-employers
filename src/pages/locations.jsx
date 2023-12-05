9f413a723a9b18e39bcae586ab62ce700b415ae7
youngderrick82 <youngderrick82@gmail.com>
2023-12-05 13:48:55 -0600

    updating locations page

---
+++ src/pages/locations.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import "./App.css";
+import Layout from '@/components/Layout';
+
+
+
+
+
+const Locations = () => {
+
+
+    return (
+
+        <Layout>
+            <div>
+                
+            </div>
+        </Layout>
+    )
+}
+
+
+
+
+export default Locations
