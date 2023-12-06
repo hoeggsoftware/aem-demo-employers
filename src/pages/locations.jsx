2feb9b3f463e32db6ce1500976fd4589a2c384a0
youngderrick82 <youngderrick82@gmail.com>
2023-12-06 12:38:58 -0600

    updated location page layout and added cards and functionality to set current office

--- src/pages/locations.jsx
+++ src/pages/locations.jsx
@@ -7,16 +7,16 @@ import Offices from "@/components/mockups/offices";
 const Locations = () => {
 
 
-  const [activeOffice, isActiveOffice] = useState("")
+  const [activeOffice, setActiveOffice] = useState("General")
 
 
-  const theFunc = () => {
-
+  const updateOffice = (office, index) => {
+    
   }
 
 
 useEffect(() => {
-  theFunc();
+  updateOffice();
 }, [activeOffice]);
 
 
@@ -26,13 +26,18 @@ useEffect(() => {
       <div className="locations-container">
         <div>
           <Card className="location-card">
-
             <h2>Offices</h2>
             <ul>
               {Offices.map((office) => (
-                <li key={office.phone}>{office.office}</li>
+                <li className="location-item" onClick={() => setActiveOffice(office.office)} key={office.phone}>{office.office}</li>
               ))}
             </ul>
+          </Card>
+        </div>
+        <div>
+          <Card className="location-info-card">
+              <h2>{Offices.office}</h2>
+              <h2>{activeOffice}</h2>
           </Card>
         </div>
       </div>
